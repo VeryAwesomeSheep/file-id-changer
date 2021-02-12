455b9d4791cfac3eb5d47a4991502eecd160e5fa
VeryAwesomeSheep <59034515+VeryAwesomeSheep@users.noreply.github.com>
2021-02-12 17:06:10 +0100

    Update FileIDchanger.py

--- FileIDchanger.py
+++ FileIDchanger.py
@@ -1,7 +1,17 @@
+"""
+Specify paths before use!
+
+If your files are using different naming scheme than "id. NAME.extension" change custom split to whatever fills your needs
+
+Program is using a dict to sort files because "475" < "5".
+"""
+
 import os
 
 sourcePath = r""
 destinationPath = r""
+
+customSplit = ". " # This can't be set to ".", all other options will work
 
 while True:
     try:
@@ -15,15 +25,15 @@ fileList = os.listdir(sourcePath)
 sortingFiles = dict()
 
 for i in fileList:
-    sortingFiles[int(i.split(". ")[0])] = i
+    sortingFiles[int(i.split(customSplit)[0])] = i
 
 sortedFiles = sorted(sortingFiles.items())
 
 for i in sortedFiles:
     for y in fileList:
         if i[1] == y:
-            oldName = y.split(". ")[1]
-            os.rename(sourcePath + "\\" + y, destinationPath + "\\" + str(startID) + ". " + oldName) 
+            oldName = y.split(customSplit)[1]
+            os.rename(sourcePath + "\\" + y, destinationPath + "\\" + str(startID) + customSplit + oldName) 
     startID += 1
 
 print("Done")
